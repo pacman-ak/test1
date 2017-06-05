a580b2463878b5c376a25194b6119cf573250575
grip-ko\ko <pacman_7788@yahoo.co.jp>
2017-06-05 12:05:50 +0900

    test40000

--- MyApplication/app/src/main/java/com/example/ko/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/ko/myapplication/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     //test2000
     //test10000
     //test20000
+    //test40000
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
